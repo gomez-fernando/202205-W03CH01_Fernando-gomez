2e74fe7efe4fd28fa7a4c5bc82e9b11b37608a79
gomez-fernando <ferdgomez@hotmail.es>
2022-05-24 08:11:36 +0200

    Add eventlistener to buttons

--- js/index.js
+++ js/index.js
@@ -15,10 +15,45 @@ import { arrayPersonajes } from "./personajes.js";
 
 
 function index() {
-    console.log(arrayPersonajes);
 
     const carrousel = document.querySelector('#carrousel');
     // carrousel.innerHTML = carrouselTemplate(arrayPersonajes[0].name, arrayPersonajes[0].family);
     carrousel.innerHTML = carrouselTemplate(arrayPersonajes);
+
+    const buttons = document.querySelectorAll('.character__action.btn');
+    // const dieButtons = document.querySelectorAll('.character__action.btn');
+    let comunications = document.querySelector('.comunications');
+
+    
+    // buttons.addEventListener('click', () => {alert("dfdf")})
+    // console.dir(comunications);
+    
+        
+                
+       
+        
+
+    
+    
+    for (let i = 0; i < buttons.length; i++) {
+        // buttons[i].addEventListener('click', (ev) => {console.log(ev.target.dataset.name)});
+        // comunications.classList.add('on');
+
+        buttons[i].addEventListener('click', (ev) => {
+            if(ev.target.dataset.action === 'habla'){
+                console.log(ev.target.dataset.action);
+                comunications.classList.add('on');
+                console.log(comunications);
+
+                setTimeout(() =>{
+                    comunications.classList.remove('on');
+                }, 2)
+            }
+        });
+    }
+
+    // const deathCharacter = (buttons) => {
+
+    // }
 
 }
